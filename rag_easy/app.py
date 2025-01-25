54b01b2ee3eb7744cdea75a37653cd4de8480f6c
Francisco Filho <francico@capimgrosso.com>
2025-01-25 08:09:05 -0300

    Renamed package to 'rag_easy' and moved files to follow PEP8 naming conventions
    - Renaming of the package from `rag-easy` to `rag_easy`.
    - Adherence to Python's PEP8 naming conventions by switching to camelCase.
    - The restructuring of the project directory, moving all relevant files under a new
    `src/rag_easy` directory.

--- rag_easy/app.py
+++ rag_easy/app.py
@@ -1,8 +1,14 @@
+import logging
 from pydantic import BaseModel
 from typing import Union
 from fastapi import FastAPI
-from embedder import OllamaEmbedder
-from db import persist_embedding, vector_query
+from rag_easy.embedder import OllamaEmbedder
+from rag_easy.db import persist_embedding, vector_query
+from rag_easy.llm import LLMOllama
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 app = FastAPI()
 embedder = OllamaEmbedder()
@@ -30,8 +36,20 @@ def search(sr: SearchRequest):
 
 @app.post("/answer")
 def answer(req: SearchRequest):
-    texts = vector_query(embedder.embed(req.text), limit=req.limit)
-    # mount a request with the top `limit` results`
-    # ask to a LLM
-    # respond to the user
-    return {"response": texts}
+    texts = vector_query(embedder.embed(req.query), limit=req.limit)
+    info = "\n\n".join([t.body for t in texts])
+
+    prompt = f"""
+    Based on the following information:
+    {info}
+
+    Answer the following question:
+    {req.query}
+    """
+    llm = LLMOllama(model="deepseek-r1:14b")
+
+    logger.info(f"Prompt: {prompt}")
+    response = llm.chat(prompt)
+    logger.info(response)
+
+    return {"response": response}
