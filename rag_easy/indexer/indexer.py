d7db8abc5f57ad6ecf73d2f579dbea32fc63e90f
Francisco Filho <francico@capimgrosso.com>
2025-01-26 10:41:50 -0300

    Add indexer module
    1. **`models.py`**: Defines Pydantic data models (`Page`, `Document`, and `Chunk`) that
    structure document content and metadata.
    2. **`indexer.py`**: Implements core functionality for loading and chunking documents:
       - `Loader` interface with a concrete implementation `PdfLoader` to read PDF files.
       - `Chunker` hierarchy, including `PageChunker` which handles splitting pages into chunks

---
+++ rag_easy/indexer/indexer.py
@@ -0,0 +1,60 @@
+from tqdm import tqdm
+from pypdf import PdfReader
+from pydantic import BaseModel
+from pathlib import Path
+from .models import Page, Document, Chunk
+
+class Chunker(BaseModel):
+    pass
+
+class PageChunker(Chunker):
+    def chunk(self, pages: list[Page], metadata={},overlap: int = 2) -> list[Chunk]:
+        result = []
+        c = 0
+        while c < len(pages):
+            text = ""
+            for i in range(overlap):
+                if c > len(pages)-overlap:
+                    text = text + pages[c].content
+                else:
+                    text = text + pages[c+i].content
+            pm = metadata.copy()
+            pm["page"] = pages[c].index
+            result.append(Chunk(index=str(pages[c].index), text=text, metadata=pm))
+            c = c + 1
+                
+        return result
+
+class Loader(BaseModel):
+    def load(self, *args, **kwargs):
+        raise NotImplementedError
+
+class PdfLoader(Loader):
+    def load(self, fp: str, first_page=0, last_page=-1, remove_footer=True) -> Document:
+        if not Path(fp).exists():
+            raise FileNotFoundError(f"File {fp} does not exist")
+        return self._read_pdf(fp, first_page, last_page, remove_footer)
+
+    def _read_pdf(self, file, first_page=24, last_page=-1, remove_footer=True) -> Document:
+        def visitor_fn(text, cm, tm, font_dict, font_size):
+            y = cm[5]
+            if 150 < y < 500 and text:
+                pages_acc.append(text)
+
+        pages = []                
+        pages_acc = []   
+        doc = PdfReader(file)
+        
+        for i, p in enumerate(tqdm(doc.pages[first_page:last_page], desc="Reading document")):
+            if remove_footer:
+                pages.append(Page(index=p.page_number, content=p.extract_text(visitor_text=visitor_fn).strip()))
+            else:
+                pages.append(Page(index=p.page_number, content=p.extract_text().strip()))
+
+        return Document(
+            title=doc.metadata.title,
+            author=doc.metadata.author,
+            subject=doc.metadata.subject,
+            date=doc.metadata.creation_date,
+            pages=pages
+        )
---
+++ rag_easy/indexer/models.py
@@ -0,0 +1,39 @@
+from typing import Union
+from datetime import datetime
+from pydantic import BaseModel
+
+class Page(BaseModel):
+    index: int
+    content: str
+
+    def summary(self) -> str:
+        max_size = 100
+        content = self.content[:max_size] + "..." if len(self.content) > max_size else self.content
+        return f"Page {self.index}:\n{content}"
+
+class Document(BaseModel):
+    title: str
+    author: str
+    date: datetime
+    subject: Union[str, None] = None
+    pages: list[Page]
+
+    def __str__(self):
+        return f"Document(title={self.title}, author={self.author}, date={self.date}, subject={self.subject})"
+
+    @property
+    def metadata(self) -> dict:
+        return {
+            "title": self.title,
+            "author": self.author,
+            "date": self.date.strftime("%Y-%m-%d"),
+            "subject": self.subject
+        }
+
+class Chunk(BaseModel):
+    index: str
+    metadata: dict
+    text: str
+
+    def __str__(self):
+        return f"Chunk(index={self.index}, metadata={self.metadata}, text={self.text[0:16]}...)"
